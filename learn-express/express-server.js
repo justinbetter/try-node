c26304ddde9e123e47ea0f5ac359d47883a293f6
justin <hj@aituyou.com>
2017-08-09 10:45:44 +0800

    init repo

---
+++ learn-express/express-server.js
@@ -0,0 +1,26 @@
+var express = require('express')
+var app = express()
+
+var myLogger = function (req, res, next) {
+  console.log('LOGGED')
+  next()
+}
+
+app.use(myLogger)
+
+var requestTime = function (req, res, next) {
+  req.requestTime = Date.now()
+  next()
+}
+
+app.use(requestTime)
+
+app.get('/', function (req, res) {
+
+  var responseText = 'Hello World!<br>'
+  responseText += '<small>Requested at: ' + req.requestTime + '</small>'
+  res.send(responseText)
+
+})
+
+app.listen(3000)
